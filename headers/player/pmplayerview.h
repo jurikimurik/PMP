fe3dee26793adbf8a8bd43a6308d5c68aabda3c0
Jurikimurik <makovskyiyurii@gmail.com>
2023-07-09 16:35:56 +0200

    Connections between buttons and slots created. + Play, volume change, volume button support created.

--- headers/player/pmplayerview.h
+++ headers/player/pmplayerview.h
@@ -1,6 +1,8 @@
 #ifndef PMPLAYERVIEW_H
 #define PMPLAYERVIEW_H
 
+#define DEBUG 1
+
 #include <QMainWindow>
 #include "pmplayermodel.h"
 
@@ -16,11 +18,25 @@ public:
     PMPlayerView(QWidget *parent = nullptr, PMPlayerModel *model = nullptr);
     ~PMPlayerView();
 
+#if DEBUG
 public slots:
     void statusChanged();
+#endif
+
+protected slots:
+    void previousMedia();
+    void playStopMedia();
+    void nextMedia();
+    void openFullscreen();
+    void openMedia();
+    void muteMedia();
+    void changeVolume(int);
+    void colorOptions();
+
 private:
     Ui::PMPlayerView *ui;
     PMPlayerModel* m_model;
 
+    void createConnections();
 };
 #endif // PMPLAYERVIEW_H
--- sources/player/pmplayerview.cpp
+++ sources/player/pmplayerview.cpp
@@ -4,18 +4,6 @@
 #include <QGraphicsVideoItem>
 #include <QMediaDevices>
 #include <QAudioDevice>
-PMPlayerView::~PMPlayerView()
-{
-    delete ui;
-}
-
-void PMPlayerView::statusChanged()
-{
-    qDebug() << m_model->player()->mediaStatus();
-    qDebug() << m_model->player()->playbackState();
-
-}
-
 
 PMPlayerView::PMPlayerView(QWidget *parent, PMPlayerModel *model)
     : QMainWindow(parent)
@@ -28,10 +16,129 @@ PMPlayerView::PMPlayerView(QWidget *parent, PMPlayerModel *model)
     if(m_model == nullptr)
         m_model = new PMPlayerModel(this);
 
+    createConnections();
 
+#if DEBUG
     connect(m_model->player(), &QMediaPlayer::mediaStatusChanged, this, &PMPlayerView::statusChanged);
+#endif
 
-    //SIMPLE TEST
+    //TEST FILE
     m_model->player()->setSource(QUrl::fromLocalFile("/Users/urijmakovskij/Desktop/audio/05 156 - Just Chill.mp3"));
-    m_model->player()->play();
+}
+
+PMPlayerView::~PMPlayerView()
+{
+    delete ui;
+}
+
+#if DEBUG
+void PMPlayerView::statusChanged()
+{
+    qDebug() << m_model->player()->mediaStatus();
+    qDebug() << m_model->player()->playbackState();
+
+}
+#endif
+
+void PMPlayerView::previousMedia()
+{
+#if DEBUG
+    qDebug() << "PMPlayerView::previousMedia()";
+#endif
+
+}
+
+void PMPlayerView::playStopMedia()
+{
+    //If something is playing right now
+    if(m_model->player()->isPlaying())
+    {
+        m_model->player()->stop();
+    } else {
+        //Otherwise
+        m_model->player()->play();
+    }
+
+    ui->playButton->setChecked(m_model->player()->isPlaying());
+
+#if DEBUG
+    qDebug() << "PMPlayerView::playStopMedia():" << m_model->player()->isPlaying();
+#endif
+}
+
+void PMPlayerView::nextMedia()
+{
+
+#if DEBUG
+    qDebug() << "PMPlayerView::nextMedia()";
+#endif
+
+}
+
+void PMPlayerView::openFullscreen()
+{
+
+#if DEBUG
+    qDebug() << "PMPlayerView::openFullscreen()";
+#endif
+
+}
+
+void PMPlayerView::openMedia()
+{
+
+#if DEBUG
+    qDebug() << "PMPlayerView::openMedia()";
+#endif
+
+}
+
+void PMPlayerView::muteMedia()
+{
+
+#if DEBUG
+    qDebug() << "PMPlayerView::muteMedia()";
+#endif
+
+    if(m_model->audioOutput()->isMuted())
+    {
+        m_model->audioOutput()->setMuted(false);
+    } else {
+        m_model->audioOutput()->setMuted(true);
+    }
+
+    ui->volumeButton->setChecked(m_model->audioOutput()->isMuted());
+
+}
+
+void PMPlayerView::changeVolume(int)
+{
+    float volume = ui->volumeSlider->value() / 100.0;
+    m_model->audioOutput()->setVolume(volume);
+
+#if DEBUG
+    qDebug() << "PMPlayerView::changeVolume(int): " << volume;
+#endif
+}
+
+void PMPlayerView::colorOptions()
+{
+
+#if DEBUG
+    qDebug() << "PMPlayerView::colorOptions()";
+#endif
+
+}
+
+
+void PMPlayerView::createConnections()
+{
+    connect(ui->openButton, &QPushButton::clicked, this, &PMPlayerView::openMedia);
+    connect(ui->backwardButton, &QPushButton::clicked, this, &PMPlayerView::previousMedia);
+    connect(ui->playButton, &QPushButton::clicked, this, &PMPlayerView::playStopMedia);
+    connect(ui->forwardButton, &QPushButton::clicked, this, &PMPlayerView::nextMedia);
+    connect(ui->fullscreenButton, &QPushButton::clicked, this, &PMPlayerView::openFullscreen);
+    connect(ui->volumeButton, &QPushButton::clicked, this, &PMPlayerView::muteMedia);
+    connect(ui->volumeSlider, &QSlider::valueChanged, this, &PMPlayerView::changeVolume);
+    connect(ui->colorsButton, &QPushButton::clicked, this, &PMPlayerView::colorOptions);
 }
